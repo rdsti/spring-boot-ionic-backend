f0bd05e91d29893bf2e4b6604b1db91d7d76e937
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2020-09-27 14:35:57 -0300

    Criando operação de instanciação

--- src/main/java/com/rdsti/cursomc/CursomcApplication.java
+++ src/main/java/com/rdsti/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.rdsti.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rdsti.cursomc.domain.Categoria;
+import com.rdsti.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null,"Informática");
+		Categoria categoria2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
