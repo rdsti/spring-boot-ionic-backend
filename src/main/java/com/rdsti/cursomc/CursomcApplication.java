534f0dc36280e6811f50167b2a0319cec540e9fd
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2020-10-03 01:37:46 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/rdsti/cursomc/CursomcApplication.java
+++ src/main/java/com/rdsti/cursomc/CursomcApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rdsti.cursomc.domain.Categoria;
+import com.rdsti.cursomc.domain.Produto;
 import com.rdsti.cursomc.repositories.CategoriaRepository;
+import com.rdsti.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -26,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null,"Informática");
 		Categoria categoria2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(p1, p2 ,p3));
+		categoria2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(categoria1));
+		p2.getCategorias().addAll(Arrays.asList(categoria1,categoria2));
+		p3.getCategorias().addAll(Arrays.asList(categoria1));
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2 ,p3));
 	}
 
 }
