7aa35243e244ffba5dd669cf143f483839e68a0f
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-02-27 11:35:09 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/rdsti/cursomc/config/SecurityConfig.java
+++ src/main/java/com/rdsti/cursomc/config/SecurityConfig.java
@@ -44,10 +44,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
-			"/estados/**",
-			"/clientes/**"
+			"/categorias/**"
 	};
+	
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/clientes/**",
