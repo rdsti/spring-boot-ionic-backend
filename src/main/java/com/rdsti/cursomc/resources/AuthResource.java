c4329a8ceb1d122023e9ed351cf2b08488c294bd
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-02-27 21:26:10 -0300

    Reflesh token

---
+++ src/main/java/com/rdsti/cursomc/resources/AuthResource.java
@@ -0,0 +1,36 @@
+package com.rdsti.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rdsti.cursomc.security.JWTUtil;
+import com.rdsti.cursomc.security.UserSS;
+import com.rdsti.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+
+}
