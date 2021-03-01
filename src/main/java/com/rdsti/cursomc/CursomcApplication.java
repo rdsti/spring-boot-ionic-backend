0b9883adef5b951aac5c36ab2e7f44cf91d80e6d
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-03-01 09:07:37 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/rdsti/cursomc/CursomcApplication.java
+++ src/main/java/com/rdsti/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.rdsti.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rdsti.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("C:\\temp\\preparar.png");
 		
 	}
 
