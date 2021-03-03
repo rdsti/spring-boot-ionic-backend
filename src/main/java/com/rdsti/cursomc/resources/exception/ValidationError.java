8864f1a85a2eff45fa7d75094c85ee675657defa
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-03-03 01:15:11 -0300

    Padronizando formato de excecoes

--- src/main/java/com/rdsti/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/rdsti/cursomc/resources/exception/ValidationError.java
@@ -9,8 +9,8 @@ public class ValidationError extends StandardError {
 	
 	private List<FieldMessage> erros = new ArrayList<>();
 
-	public ValidationError(Integer status, String name, Long timeStamp) {
-		super(status, name, timeStamp);
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErros() {
